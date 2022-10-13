4234863f5ee1466be7196b659681c8d1b7c99f1c
BossDaily <gboys2008@gmail.com>
2022-10-13 10:50:09 -0500

    copy pasted old code from observerbot for member sub cmd

--- src/commands/info.ts
+++ src/commands/info.ts
@@ -180,8 +180,62 @@ module.exports = {
           await wait(1);
           await interaction.editReply({ embeds: [embed] });
           break;
-        case "member": 
-         
+        case "member":
+          const member = interaction.options.getMember("user");
+          const user = interaction.options.getUser("user");
+
+          const gld = interaction.guild;
+
+          const roles = member.roles.cache;
+          const roleCount = roles.size;
+          const rolesList = roles.map((role) => `<@&${role.id}>`).join(", ");
+
+          const undRemove = /(_)/g;
+
+          const flag = await user.fetchFlags();
+          const flagList = flag
+            .toArray()
+            .map(
+              (fe) =>
+                `<:icons_Correct:859388130411282442> ${fe
+                  .replace(undRemove, " ")
+                  .toLowerCase()
+                  .replace(/(^|\s)\S/g, (L) => L.toUpperCase())}`
+            )
+            .join("\n");
+
+          const embed = new MessageEmbed()
+            .setColor(`${embedColor}`)
+            .setTitle(`Information about ${member.displayName}`)
+            .addFields(
+              {
+                name: "<:icons_calendar1:941679946760351794> Joined Discord",
+                value: `<t:${Math.round(
+                  user.createdTimestamp / 1000
+                )}:F> or <t:${Math.round(user.createdTimestamp / 1000)}:R>`,
+                inline: true,
+              },
+              {
+                name: `<:icons_clock:964491800465276940> Joined ${gld.name}`,
+                value: `<t:${Math.round(
+                  member.joinedTimestamp / 1000
+                )}:F> or <t:${Math.round(member.joinedTimestamp / 1000)}:R>`,
+                inline: true,
+              },
+              {
+                name: "<:icons_dfuchsia:875710295081910292> Roles",
+                value: `${rolesList}`,
+                inline: true,
+              },
+              {
+                name: "<:icons_eventcolour:870646213429563445> Badges",
+                value: `${flagList ? flagList : "None"}`,
+                inline: true,
+              }
+            )
+            .setThumbnail(member.displayAvatarURL({ dynamic: true }));
+
+          await interaction.reply({ embeds: [embed] });
       }
     } catch (error) {
       await interaction.reply({
