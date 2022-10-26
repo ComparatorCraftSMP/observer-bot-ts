1bd38ab7ce456b7194fd5a82ecf577c3a7325003
gboys2008@gmail.com <gboys2008@gmail.com>
2022-10-26 11:22:20 -0400

    more strict type guards for channels

--- src/commands/acceptApplication.ts
+++ src/commands/acceptApplication.ts
@@ -18,10 +18,10 @@ import {
   ContextMenuCommandBuilder,
   ApplicationCommandType,
   MessageContextMenuCommandInteraction,
+  ChannelType,
 } from "discord.js";
 
 import { client } from "../../index";
-
 
 module.exports = {
   data: new ContextMenuCommandBuilder()
@@ -29,41 +29,45 @@ module.exports = {
     .setType(ApplicationCommandType.Message),
 
   async execute(interaction: MessageContextMenuCommandInteraction) {
-    
-    const msg =  interaction.targetMessage;
-    const cmdUser =  interaction.member;
-    const applicant =  interaction.targetMessage.member;
-    
+    const msg = interaction.targetMessage;
+    const cmdUser = interaction.member;
+    const applicant = interaction.targetMessage.member;
+
     try {
-      if (msg.channel.parentId === application.ticket_category) {
-        if (
-          cmdUser.roles.cache.find(
-            (role) => role.id === application.staff_role
-          ) ||
-          cmdUser.permissions.has("MANAGE_ROLES")
-        ) {
-          if (applicant.user.bot) {
+      if (msg.channel.type !== ChannelType.DM) {
+        if (msg.channel.parentId === application.ticket_category) {
+          if (
+            cmdUser.roles.cache.find(
+              (role) => role.id === application.staff_role
+            ) ||
+            cmdUser.permissions.has("MANAGE_ROLES")
+          ) {
+            if (applicant.user.bot) {
+              await interaction.reply({
+                content:
+                  "This is a bot, please run this command on the application not a bot message",
+                ephemeral: true,
+              });
+            }
+            applicant.roles.add(application.member_role);
+            applicant.roles.remove(application.applicant_role);
+            await msg.reply({ content: `${application.message}` });
             await interaction.reply({
-              content:
-                "This is a bot, please run this command on the application not a bot message",
+              content: "Message sent",
+              ephemeral: true,
+            });
+          } else {
+            await interaction.reply({
+              content: "You dont have permission to send this command",
               ephemeral: true,
             });
           }
-          applicant.roles.add(application.member_role);
-          applicant.roles.remove(application.applicant_role);
-          await msg.reply({ content: `${application.message}` });
-          await interaction.reply({ content: "Message sent", ephemeral: true });
         } else {
           await interaction.reply({
-            content: "You dont have permission to send this command",
+            content: "This isn't an application",
             ephemeral: true,
           });
         }
-      } else {
-        await interaction.reply({
-          content: "This isn't an application",
-          ephemeral: true,
-        });
       }
     } catch (error) {
       await interaction.reply({
