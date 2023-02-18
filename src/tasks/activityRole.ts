9575f5dca242a485ea713d0488cdddf35bed8bf9
BossDaily <gboys2008@gmail.com>
2023-02-18 13:15:20 -0600

    basic fetch for the roles and guild

--- src/tasks/activityRole.ts
+++ src/tasks/activityRole.ts
@@ -1,4 +1,5 @@
 
+import { Guild } from 'discord.js';
 import cron from 'node-cron';
 import fetch from 'node-fetch';
 import { client } from '../..';
@@ -8,8 +9,10 @@ import { client } from '../..';
 export default async function(): Promise<void> {
   // Schedule a job to run every day at 3 PM
   cron.schedule('* * * * *', async () => {
-    const guild = await client.guilds.fetch('761670547196739635')
-    console.log(guild)
+    const guild: Guild = await client.guilds.fetch('761670547196739635')
+
+    const roles = guild.roles.fetch('1076582329243406347')
+    console.log(roles)
 
     console.log('Task 2 is running');
   });
