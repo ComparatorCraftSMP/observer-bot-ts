75fc12610211493afd0e0e180c875cffb9c99bf4
BossDaily <gboys2008@gmail.com>
2023-02-18 13:11:38 -0600

    function is now async

--- src/tasks/activityRole.ts
+++ src/tasks/activityRole.ts
@@ -5,10 +5,10 @@ import { client } from '../..';
 
 
 
-export default function(): void {
+export default async function(): Promise<void> {
   // Schedule a job to run every day at 3 PM
-  cron.schedule('* * * * *', () => {
-    const guild = client.guilds.fetch('761670547196739635')
+  cron.schedule('* * * * *', async () => {
+    const guild = await client.guilds.fetch('761670547196739635')
     console.log(guild)
 
     console.log('Task 2 is running');
