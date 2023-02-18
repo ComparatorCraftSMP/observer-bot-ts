cf642952003acf6c38c716138973eef67060e0b4
BossDaily <gboys2008@gmail.com>
2023-02-18 13:06:35 -0600

    fetches guild

--- src/tasks/activityRole.ts
+++ src/tasks/activityRole.ts
@@ -1,9 +1,16 @@
 
 import cron from 'node-cron';
+import fetch from 'node-fetch';
+import { client } from '../..';
+
+
 
 export default function(): void {
   // Schedule a job to run every day at 3 PM
   cron.schedule('* * * * *', () => {
+    const guild = client.guilds.fetch('761670547196739635')
+    console.log(guild)
+
     console.log('Task 2 is running');
   });
 };
