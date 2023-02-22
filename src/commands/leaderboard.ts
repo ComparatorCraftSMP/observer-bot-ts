eeadf8cbab4bc2a8e72df745558ee9d0e0ea3ac7
gboys2008@gmail.com <gboys2008@gmail.com>
2023-02-22 16:59:06 +0000

    commented out old logic for pagination (attempt)

--- src/commands/leaderboard.ts
+++ src/commands/leaderboard.ts
@@ -13,6 +13,7 @@ import {
   MessageActionRowComponentBuilder,
   AutocompleteInteraction,
   ChatInputCommandInteraction,
+  Embed,
 } from "discord.js";
 import fetch from "cross-fetch";
 import { config } from "../../config";
@@ -79,16 +80,67 @@ module.exports = {
         options
       );
       const data = await response.json();
+
+      const leaderboard = data.scores.sort(
+        (a: any, b: any) => b.value - a.value
+      );
+
+      let userIGN = "";
+      userIGN = Object.values(leaderboard)
+        .map((ign: any) => ign.entry)
+        .join("\n");
+      let score = "";
+      score = Object.values(leaderboard)
+        .map((score: any) => score.value)
+        .join("\n");
+
       
+
+      const pages = [];
+
+      for (let i = 0; i < leaderboard.length; i += 10) {
+        const page = leaderboard.slice(i, i + 10);
+        pages.push(page);
+      }
+
+      const embeds = [];
+      for (let i = 0; i < pages.length; i++) {
+        const embed = new EmbedBuilder()
+          .setTitle("Scores")
+          .setDescription(pages[i].join("\n"))
+          .setFooter(`Page ${i + 1} of ${pages.length}`);
+        embeds.push(embed);
+      }
+
+      /* 
       const embed = new EmbedBuilder()
         // @ts-ignore
         .setColor(`${config.embedColor}`)
-        .setTitle(`Player stats of ${username}`)
-        .setThumbnail(`https://minotar.net/armor/bust/${username}/100.png`)
-        .setDescription(`${statName}: ${statObj ? statObj.value : "0"}`);
+        .setTitle(`Statistic Name`)
+        .setDescription(`Top 15 people in ${statName}`)
+        .addFields(
+          {
+            name: "Rank",
+            value:
+              "`#1\n#2\n#3\n#4\n#5\n#6\n#7\n#8\n#9\n#10\n#11\n#12\n#13\n#14\n#15`",
+            inline: true,
+          },
+          {
+            name: "Player",
+            value: `\`${userIGN}\``,
+            inline: true,
+          },
+          {
+            name: "Value",
+            value: `\`${score}\``,
+            inline: true,
+          }
+        );
+
+      await interaction.reply({ embeds: [embed] }); */
 
       await interaction.reply({
-        embeds: [embed],
+        content: "this command is in development",
       });
     } catch (error) {
       await interaction.reply({
