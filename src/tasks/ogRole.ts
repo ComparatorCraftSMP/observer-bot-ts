213991c792780bccfacbf2c50e9c122ad72a14a5
BossDaily <gboys2008@gmail.com>
2023-02-25 13:19:51 -0600

    proper change

--- src/tasks/ogRole.ts
+++ src/tasks/ogRole.ts
@@ -8,40 +8,21 @@ export default async function (): Promise<void> {
   // Schedule a job to run every day at 3 PM
   cron.schedule("* * * * *", async () => {
     if (config.active_role.enabled) {
-      const playersQuery = await fetch(
-        `${config.plan.url}/v1/players?server=${config.plan.server}}`,
-        {
-          method: "GET",
-        }
-      );
-      const players = await playersQuery.json();
-
-      const playerData = players.data;
 
       const guild: Guild = await client.guilds.fetch(config.guild_id);
       const members = await guild.members.fetch();
 
-      const role = await guild.roles.fetch(config.active_role.member_role_id);
+      const role = await guild.roles.fetch(config.og_role.member_role_id);
 
       const roleMember = role?.members.forEach((member) => {
-        const player = playerData.find(
-          (player: any) =>
-            player.username.d.replace("@", "") === member.user.tag
-        );
-        if (!player) return;
-
-        const activity = new Number(player.index.v);
-        if (activity >= 2) {
-          if (!member.roles.cache.has(config.active_role.role_id)) {
-            member.roles.add(config.active_role.role_id);
-            console.log(` gave ${member.user.tag} activity role`);
-          }
-        }
-
-        if (activity < 2) {
-          if (member.roles.cache.has(config.active_role.role_id)) {
-            member.roles.remove(config.active_role.role_id);
-            console.log(` removed ${member.user.tag} activity role`);
+        // @ts-ignore
+        const memberJoined = member.joinedTimestamp / 1000
+        const timestamp = config.og_role.joined_before
+        // @ts-ignore
+        if (memberJoined < timestamp) {
+          if (!member.roles.cache.has(config.og_role.role_id)) {
+            member.roles.add(config.og_role.role_id);
+            console.log(` gave ${member.user.tag} og role`);
           }
         }
       });
