6ad22326afb4d50f3a11c4802887c05590c7ea46
BossDaily <gboys2008@gmail.com>
2023-02-26 08:07:08 -0600

    proper embed and querying data

--- src/commands/playerlist.ts
+++ src/commands/playerlist.ts
@@ -15,6 +15,7 @@ import {
 } from "discord.js";
 
 import { config } from "../../config";
+import fetchPlaceholder from "../utils/fetchPlaceholder";
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -28,27 +29,27 @@ module.exports = {
         headers: { Accept: "application/json", key: `${process.env.API}` },
       };
 
-      const response = await fetch(`${process.env.SERVER}/v1/plugins`, options);
-      const data = await response.json();
-      let pluginString = "";
-      pluginString = Object.values(data)
-        .map((plugin: any) => {
-          if(plugin.website) {
-            return `[${plugin.name}](${plugin.website})`;
-          } else {
-            return plugin.name;
-          }
+      const playerRes = await fetch(`${process.env.SERVER}/v1/players`, options);
+      const playerData = await playerRes.json();
+
+      const serverRes = await fetch(`${process.env.SERVER}/v1/server`, options);
+      const serverData = await serverRes.json();
+
+      let playerString = "";
+      playerString = Object.values(playerData)
+        .map((player: any) => {
+          return player.displayName
         })
         .join(", ");
 
       const embed = new EmbedBuilder()
         // @ts-ignore
         .setColor(config.embedColor)
-        .setTitle(`${interaction.guild?.name}'s plugins`)
-        .setDescription(`${pluginString}`)
+        .setTitle(`${interaction.guild?.name}'s playerlist`)
+        .setDescription(`${await fetchPlaceholder('0e4735e7-b910-480e-9566-e83d960c978d', '%playerlist_online,normal,yes,list%')}`)
         // @ts-ignore
         .setThumbnail(interaction.guild?.iconURL())
-        .setFooter({ text: `${interaction.guild?.name} has ${data.length} plugins`});
+        .setFooter({ text: `${serverData.onlinePlayers}/${serverData.maxPlayers} players online`});
 
       await interaction.reply({ embeds: [embed] });
     } catch (error) {
