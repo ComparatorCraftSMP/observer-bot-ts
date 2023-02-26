b377c562eb3a6ae5d62965607e7b1d53032afb61
BossDaily <gboys2008@gmail.com>
2023-02-26 15:02:35 -0600

    changed config name and import

--- src/delete-commands.ts
+++ src/delete-commands.ts
@@ -1,13 +1,16 @@
 import { SlashCommandBuilder } from "@discordjs/builders";
-import { REST, Routes } from "discord.js"
+import { REST, Routes } from "discord.js";
 import { client } from "../index";
-import  config  from "../config";
+import configg from "../config";
 import dotenv, { config } from "dotenv";
 
+dotenv.config();
 
-  const rest = new REST({ version: "10" }).setToken(process.env.TOKEN!);
+const rest = new REST({ version: "10" }).setToken(process.env.TOKEN!);
 
-  rest
-    .put(Routes.applicationGuildCommands(config.clientId, config.guild_id), { body: [] })
-    .then(() => console.log(`Successfully deleted all application commands.`))
-    .catch(console.error);
+rest
+  .put(Routes.applicationGuildCommands(configg.clientId, configg.guild_id), {
+    body: [],
+  })
+  .then(() => console.log(`Successfully deleted all application commands.`))
+  .catch(console.error);
