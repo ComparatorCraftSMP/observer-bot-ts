9a84b15e92d0934df6cda83b0e9fefb088626007
BossDaily <gboys2008@gmail.com>
2023-02-26 15:04:01 -0600

    removed function

--- src/deploy-commands.ts
+++ src/deploy-commands.ts
@@ -8,37 +8,30 @@ import { REST } from "@discordjs/rest";
 import { Routes } from "discord-api-types/v10";
 import { client } from "../index";
 import dotenv from "dotenv";
-import { config } from "../config";
+import config from "../config";
 
 dotenv.config();
-export const regCMD = () => {
-  // dotenv dependancy
 
-  //Gets slash commands
-  const commands: any[] = [];
-  const cmdPath = path.join(__dirname, "commands");
-  const cmdFiles = fs
-    .readdirSync(cmdPath)
-    .filter((file) => file.endsWith(".js"));
+// dotenv dependancy
 
-  const rest = new REST({ version: "10" }).setToken(process.env.TOKEN!);
+//Gets slash commands
+const commands: any[] = [];
+const cmdPath = path.join(__dirname, "commands");
+const cmdFiles = fs.readdirSync(cmdPath).filter((file) => file.endsWith(".js"));
 
-  for (const file of cmdFiles) {
-    const filePath = path.join(cmdPath, file);
-    const command = require(filePath);
-    commands.push(command.data.toJSON());
-  }
+const rest = new REST({ version: "10" }).setToken(process.env.TOKEN!);
 
-  rest
-    .put(Routes.applicationCommands(config.clientId), { body: commands })
-    .then(() =>
-      console.log(
-        `Successfully registered ${commands.length} application commands.`
-      )
+for (const file of cmdFiles) {
+  const filePath = path.join(cmdPath, file);
+  const command = require(filePath);
+  commands.push(command.data.toJSON());
+}
+
+rest
+  .put(Routes.applicationCommands(config.clientId), { body: commands })
+  .then(() =>
+    console.log(
+      `Successfully registered ${commands.length} application commands.`
     )
-    .catch(console.error);
-};
-
-
-
-
+  )
+  .catch(console.error);
