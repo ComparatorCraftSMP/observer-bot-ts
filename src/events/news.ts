f690dbfbe32801b532d6b9b95b0de810989011e9
BossDaily <gboys2008@gmail.com>
2023-02-26 14:34:26 -0600

    added embed for specifying where attachments and embeds came from

--- src/events/news.ts
+++ src/events/news.ts
@@ -5,6 +5,7 @@ import {
   InteractionType,
   ChannelType,
   Message,
+  resolveColor,
 } from "discord.js";
 import { config } from "../../config";
 
@@ -25,20 +26,73 @@ module.exports = {
 
         if (sendChannel?.isTextBased()) {
           if (msg.content) {
-            sendChannel?.send({ content: msg.content });
+            const embed = new EmbedBuilder()
+              .setAuthor({
+                name: msg.author.username,
+                // @ts-ignore
+                iconURL: msg.author.avatarURL(),
+              })
+              // @ts-ignore
+              .setColor(resolveColor(config.embedColor))
+              .setDescription(msg.content)
+              .setTimestamp(msg.createdAt);
+
+            sendChannel?.send({ embeds: [embed] });
           }
 
           if (msg.attachments) {
+            if (!msg.content && msg.attachments.size > 0) {
+              const embed = new EmbedBuilder()
+                .setAuthor({
+                  name: msg.author.username,
+                  // @ts-ignore
+                  iconURL: msg.author.avatarURL(),
+                })
+                // @ts-ignore
+                .setColor(resolveColor(config.embedColor))
+                .setDescription(`${msg.author.username} attached:`);
+
+              sendChannel?.send({ embeds: [embed] });
+            }
+
             msg.attachments.map((attch) =>
               sendChannel?.send({ files: [attch] })
             );
           }
 
           if (msg.embeds) {
+            if (!msg.content && msg.embeds.length > 0) {
+              const embed = new EmbedBuilder()
+                .setAuthor({
+                  name: msg.author.username,
+                  // @ts-ignore
+                  iconURL: msg.author.avatarURL(),
+                })
+                // @ts-ignore
+                .setColor(resolveColor(config.embedColor))
+                .setDescription(`${msg.author.username} sent:`);
+
+              sendChannel?.send({ embeds: [embed] });
+            }
+
             msg.embeds.map((embed) => sendChannel?.send({ embeds: [embed] }));
           }
 
           if (msg.stickers) {
+            if (!msg.content && msg.stickers.size > 0) {
+              const embed = new EmbedBuilder()
+                .setAuthor({
+                  name: msg.author.username,
+                  // @ts-ignore
+                  iconURL: msg.author.avatarURL(),
+                })
+                // @ts-ignore
+                .setColor(resolveColor(config.embedColor))
+                .setDescription(`${msg.author.username} sent some stickers!`);
+
+              sendChannel?.send({ embeds: [embed] });
+            }
+
             msg.stickers.map((sticker) =>
               sendChannel?.send({ files: [sticker.url] })
             );
